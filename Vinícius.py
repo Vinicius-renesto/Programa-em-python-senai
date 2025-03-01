7ff6ac85760810c34f8e9da434eb8676728941a0
Vinicius-renesto <vini.reenesto@gmail.com>
2025-02-28 21:55:00 -0300

    Add files via upload

---
+++ Vinícius.py
@@ -0,0 +1,21 @@
+numero = 10
+print(type(numero))
+mudou_1 = float(numero)
+print(type(mudou_1))
+
+texto = 'salve mundo, sou louco'
+print(type(texto))
+mudou_2 = bool(texto)
+print(type(mudou_2))
+
+logico = True
+print(type(logico))
+mudou_3 = int(logico)
+print(type(mudou_3))
+
+real = 5.2
+print(type(real))
+mudou_4 =str(real)
+print(type(mudou_4))
+tipo = type(real)
+print(tipo)
