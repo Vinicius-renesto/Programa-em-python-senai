749befb9ae969c5dcb1750b9794ce2bcf024ef3c
Vinicius-renesto <vini.reenesto@gmail.com>
2025-03-05 21:57:57 -0300

    Add files via upload

---
+++ Vinícius-Aula-5.py
@@ -0,0 +1,84 @@
+#execício 1
+nome = input('qual seu nome')
+print(f'ola {nome}, seja bem-vindo')
+
+#execício 2
+numero = input (f'Digite um numero {nome}?')
+verdadeiro = True
+tipo = type(verdadeiro)
+print('tipo{}'.format(tipo))
+
+#execício 3
+numero1 = float(input(f'Digite um numero para a multiplicação {nome}?'))
+numero2 = float(input(f'Digite um outro numero para a multiplicação {nome}?'))
+resultado = numero1 * numero2
+print(nome,'seu resultado é',resultado)
+
+#execício 4
+numero3 = int (input (f'Digite um numero para a divisão {nome}?'))
+numero4 = int (input (f'Digite um outro numeropara a divisão {nome}?'))
+resultado = numero3/numero4
+print('seu resultado é %s'%(resultado))
+
+#execício 5
+numero5 = int (input (f'Digite um numero para a subtração {nome}?'))
+numero6 = int (input (f'Digite um outro numeropara a subtração {nome}?'))
+resultado = numero5 - numero6
+print('seu resultado é %s'%(resultado))
+
+#execício 6
+numero7 = int (input (f'Digite um numero para a divisão {nome}?'))
+numero8 = int (input (f'Digite um outro numeropara a divisão {nome}?'))
+resultado = numero7 / numero8
+print('seu resultado é %s'%(resultado))
+
+#execício 7
+numero9 = float (input (f'Digite um numero para a multiplicação {nome}?'))
+numero10 = float (input (f'Digite um outro numeropara a multiplicação {nome}?'))
+numero11 = float(input(f'Digite um numero para a multiplicação {nome}?'))
+numero12 = float(input(f'Digite um outro numero para a multiplicação {nome}?'))
+resultado = numero9 * numero10 * numero11 * numero12
+print(nome,'seu resultado é',resultado)
+
+#execício 8
+numero13 = int (input (f'Digite um numero para o dobro {nome}?'))
+resultado = numero13 ** 2
+print(nome,'seu resultado é',resultado)
+
+#execício 9
+# soma
+numero14 = int (input (f'Digite um numero para o dobro {nome}?'))
+numero15 = int (input (f'Digite um numero para o dobro {nome}?'))
+resultado = numero14 + numero15
+print(nome,'seu resultado é',resultado)
+
+# subtração
+numero16 = int (input (f'Digite um numero para o dobro {nome}?'))
+numero17 = int (input (f'Digite um numero para o dobro {nome}?'))
+resultado = numero16 - numero17
+print(nome,'seu resultado é',resultado)
+
+# multiplicação
+numero18 = int (input (f'Digite um numero para o dobro {nome}?'))
+numero19 = int (input (f'Digite um numero para o dobro {nome}?'))
+resultado = numero18 * numero19
+print(nome,'seu resultado é',resultado)
+
+#divisao
+numero20 = int (input (f'Digite um numero para o dobro {nome}?'))
+numero21 = int (input (f'Digite um numero para o dobro {nome}?'))
+resultado = numero20 / numero21
+print(nome,'seu resultado é',resultado)
+
+#execício 10
+nome1 = input('qual seu nome?')
+sobrenome = input('qual seu sobrenome?')
+email = input('qual seu email?')
+senha = input('qual seu senha?')
+corfimar_senha = input('confirme sua senha')
+
+print(f'seja bem-vindo {nome1} s sua senha é {senha}')
+
+
+
+
